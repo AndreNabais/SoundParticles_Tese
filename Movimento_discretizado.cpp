69612a514da947895e67f74d26f97245c2d2e455
AndreNabais <andrefnabais@gmail.com>
2026-04-27 14:59:41 +0100

    Update and rename prototipo2_3D.cpp to Movimento_discretizado.cpp

--- Movimento_discretizado.cpp
+++ Movimento_discretizado.cpp
@@ -136,45 +136,22 @@ class Microphones {
         return make_pair(micLeft, micRight);
         }
     };
-    /*
 
-    vector<float> getLeftTime() const {
-        return micLeftTime;
-    }
-    vector<float> getRightTime() const {
-        return micRightTime;
-    }
-};
-
-*/
 
 int main(){
     //let's simulate the discretized movement of the microphones throughout 3 different x positions
     Source src(0.5, 440.0, 44100, 512);
     Environment env1(0, -1.0, 1.0, 0, 0, 0, 0, 0, 0, 343); //for now change x over constant y and z
-    Environment env2(0, 1.0, 5.0, 0, 0, 0, 0, 0, 0, 343);
-    Environment env3(0, 4.0, 12.0, 0, 0, 0, 0, 0, 0, 343);
+    Environment env2(0, -3.0, 5.0, 0, 0, 0, 0, 0, 0, 343);
+    Environment env3(0, -5.0, 12.0, 0, 0, 0, 0, 0, 0, 343);
     Microphones mic;
     auto buffer = src.buffer_source();
-    auto buffer_time = src.time_source();
-    env1.DelayTime();
-    env2.DelayTime();
-    env3.DelayTime();
-    env1.DelaySample(src);
-    env2.DelaySample(src);
-    env3.DelaySample(src);
-    auto [micLeft1, micRight1] = mic.capture(src, env1);
-    auto [micLeft2, micRight2] = mic.capture(src, env2);
-    auto [micLeft3, micRight3] = mic.capture(src, env3);
-    cout << "Samples at microphone on the left in the 1st interval: " << micLeft1.size() << endl;
-    cout << "Samples at microphone on the right in the 1st interval: " << micRight1.size() << endl;
-    cout << "Samples at microphone on the left in the 2nd interval: " << micLeft2.size() << endl;
-    cout << "Samples at microphone on the right in the 2nd interval: " << micRight2.size() << endl;   
-    cout << "Samples at microphone on the left in the 3rd interval: " << micLeft3.size() << endl;
-    cout << "Samples at microphone on the right in the 3rd interval: " << micRight3.size() << endl;
+    float offset = 0.03; //offset of 30ms to ensure we have enough samples for the delay at the beginning of the signal
+    int nr_positions = 3;
+
 
     //compute velocity_left and velocity_right throughout total_time
-    float total_time = static_cast<float>(buffer.size())/static_cast<float>(src.getSampleRate());
+    float total_time = nr_positions*(static_cast<float>(buffer.size())/static_cast<float>(src.getSampleRate())) + (nr_positions-1)*offset; //total time of the signal including the offset between bursts
     float velocity_left = ((env3.get_x_left()-env1.get_x_left())/total_time);  //x_start = env1.get_x_left() and x_end = env3.get_x_left()
     float velocity_right = ((env3.get_x_right()-env1.get_x_right())/total_time);  //x_start = env1.get_x_right() and x_end = env3.get_x_right()
 
@@ -182,74 +159,65 @@ int main(){
     //save csv files
 
     ofstream src_file("./source.csv");
-    for (size_t i = 0; i < buffer.size(); ++i) {
-        float time_source = static_cast<float>(i)/static_cast<float>(src.getSampleRate());
-         src_file << time_source << "," << buffer[i] << "\n";       
+    for (size_t n = 0; n < nr_positions; n++) {    
+        float burst_start_time = n * (static_cast<float>(buffer.size())/static_cast<float>(src.getSampleRate())+offset);
+        for (size_t i = 0; i < buffer.size(); ++i) {
+            float time_source = static_cast<float>(i)/static_cast<float>(src.getSampleRate()) + burst_start_time;
+            src_file << time_source << "," << buffer[i] << "\n";
+            };
+            
         };
 
     src_file.close();
 
 
-    // Combine the left and right mic into one long signal (simulating motion)
-    vector<float> micLeft_all;
-    micLeft_all.insert(micLeft_all.end(), micLeft1.begin(), micLeft1.end());
-    micLeft_all.insert(micLeft_all.end(), micLeft2.begin(), micLeft2.end());
-    micLeft_all.insert(micLeft_all.end(), micLeft3.begin(), micLeft3.end());
-
-    vector<float> micRight_all;
-    micRight_all.insert(micRight_all.end(), micRight1.begin(), micRight1.end());
-    micRight_all.insert(micRight_all.end(), micRight2.begin(), micRight2.end());
-    micRight_all.insert(micRight_all.end(), micRight3.begin(), micRight3.end());
-
-
-    // Make a time axis for plotting
-    vector<float> time_left(micLeft_all.size());
-    for (size_t i = 0; i < time_left.size(); ++i)
-        time_left[i] = static_cast<float>(i) / static_cast<float>(src.getSampleRate());
-
-    vector<float> time_right(micRight_all.size());
-    for (size_t i = 0; i < time_right.size(); ++i)
-        time_right[i] = static_cast<float>(i) / static_cast<float>(src.getSampleRate());
-
     string filenames[2] = {"./mic_left.csv", "./mic_right.csv"};
     ofstream ofs;
 
+
+
     //left_file
-    //ver problema na escrita, micrightall tá a dar valores mas n ta a salvar devidamente no ficheiro csv
     ofs.open(filenames[0]);
-        for (size_t i = 0; i < time_left.size(); ++i) {
-            float x_mic_left = env1.get_x_left() + velocity_left*time_left[i];
-            Environment env_moving(0, x_mic_left, 0, 0, 0, 0, 0, 0, 0, 343);
-            env_moving.DelayTime();
-            auto [delaySampleLeft, delaySampleRight] = env_moving.DelaySample(src);
-            if ((i-delaySampleLeft) < 0) {
-                micLeft_all[i] = 0;
-                }
-            else {
-                micLeft_all[i] = buffer[i-delaySampleLeft];
-                }
-            ofs << time_left[i] << "," << micLeft_all[i] << "\n"; 
-        };
+
+        for (size_t n = 0; n < nr_positions; n++) {    
+            float burst_start_time = n * (static_cast<float>(buffer.size())/static_cast<float>(src.getSampleRate())+offset);
+
+            for (size_t i = 0; i < buffer.size(); ++i) {
+                float global_time = static_cast<float>(i)/static_cast<float>(src.getSampleRate()) + burst_start_time;
+                float x_mic_left = env1.get_x_left() + velocity_left*global_time;
+                Environment env_moving(0, x_mic_left, 0, 0, 0, 0, 0, 0, 0, 343);
+                auto [delayLeft, _] = env_moving.DelayTime();
+                float time_left = delayLeft + global_time;
+                float amplitude_left = buffer[i];
+
+                ofs << time_left << "," << amplitude_left << "\n"; 
+                };
+            };
         ofs.close();
         cout << "Saved mic_left.csv with simulated movement!" << endl;
 
         //right_file
     ofs.open(filenames[1]);
-        for (size_t i = 0; i < time_right.size(); ++i) {
-            float x_mic_right = env1.get_x_right() + velocity_right*time_right[i];
-            Environment env_moving(0, 0, x_mic_right, 0, 0, 0, 0, 0, 0, 343);
-            env_moving.DelayTime();
-            auto [delaySampleLeft, delaySampleRight] = env_moving.DelaySample(src);
-            if ((i-delaySampleRight) < 0) {
-                micRight_all[i] = 0;
-                }
-            else {
-                micRight_all[i] = buffer[i-delaySampleRight];
-                }
-            ofs << time_right[i] << "," << micRight_all[i] << "\n"; 
-        };
+
+
+            for (size_t n = 0; n < nr_positions; n++) {    
+            float burst_start_time = n * (static_cast<float>(buffer.size())/static_cast<float>(src.getSampleRate())+offset);
+
+            for (size_t i = 0; i < buffer.size(); ++i) {
+                float global_time = static_cast<float>(i)/static_cast<float>(src.getSampleRate()) + burst_start_time;
+                float x_mic_right = env1.get_x_right() + velocity_right*global_time;
+                Environment env_moving(0, 0, x_mic_right, 0, 0, 0, 0, 0, 0, 343);
+                auto [_, delayRight] = env_moving.DelayTime();
+                float time_right = delayRight + global_time;
+                float amplitude_right = buffer[i];
+
+                ofs << time_right << "," << amplitude_right << "\n"; 
+                };
+            };
         ofs.close();
         cout << "Saved mic_right.csv with simulated movement!" << endl;
+
+
     
 
 
